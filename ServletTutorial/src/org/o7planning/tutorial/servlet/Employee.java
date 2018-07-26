8ece0417d26f4e43c7b533d14c2a3269a6fb74cf
sudevmandal <sudev.mandal2504@gmail.com>
2018-07-26 15:25:44 +0530

    added project

---
+++ ServletTutorial/src/org/o7planning/tutorial/servlet/Employee.java
@@ -0,0 +1,29 @@
+package org.o7planning.tutorial.servlet;
+
+public class Employee {
+	 
+	  private String empNo;
+	  private String empName;
+	 
+	  public Employee() {
+	 
+	  }
+	 
+	  public String getEmpNo() {
+	      return empNo;
+	  }
+	 
+	  public void setEmpNo(String empNo) {
+	      this.empNo = empNo;
+	  }
+	 
+	  public String getEmpName() {
+	      return empName;
+	  }
+	 
+	  public void setEmpName(String empName) {
+	      this.empName = empName;
+	  }
+	 
+	}
+
